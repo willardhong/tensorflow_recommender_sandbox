81624eb568fd042f150e1cf87c47d5515c8ae883
willardhong <willmimi@yahoo.com>
2022-08-27 23:47:47 -0700

    3rd iteration

--- index.js
+++ index.js
@@ -19,36 +19,78 @@ function recomPick(id) {        ;
     const p = document.getElementById(listButton[0][pick]);
     //console.log(p.className);
     p.className = 'btn btn-primary';
+    // add onclick to the next col                 
+    for (let i=1; i<=8; i++) {
+        myElement = 'but1' + i;
+        const element = document.getElementById(myElement);
+        element.setAttribute('onclick', 'selectPick(this)');
+        //console.log(element);
+        
+    }
 }
 // track selection against recommendation
 function selectPick(id) {
     //console.log(id.id);
     const x = document.getElementById(id.id);
+    const thiscol = parseInt(x.id.substr(3,1));
+    const nextcol = thiscol + 1;
+    
     //console.log(parseInt(x.id.substr(3, 1)));
-    console.log(x.className);
+    //console.log(x.className);
     if (x.className == 'btn btn-primary') {
          x.className = 'btn btn-success';
     } else {
         
         x.className = 'btn btn-danger';
     }
+    //remove all onclick from current col
+    for (let i=1; i<=8; i++) {
+        myElement = 'but'+ thiscol + i;
+        const element = document.getElementById(myElement);
+        element.removeAttribute('onclick');
+        //console.log(element);
+        
+    }
     
+    // simulate recom using random integer
     let pick = Math.floor(Math.random()*(8-1+1)+1)-1;
-    //console.log(pick);
-   
-    const p = document.getElementById(listButton[parseInt(x.id.substr(3, 1))][pick]);
-    //console.log(p.className);
-    p.className = 'btn btn-primary';
+    if (thiscol <= 7) {
+        const p = document.getElementById(listButton[thiscol][pick]);
+        //console.log(p.className);
+        p.className = 'btn btn-primary';
+    }
+    // add onclick to the next col    
+    console.log('nextcol = ' + nextcol);
+    for (let i=1; i<=8; i++) {
+        myElement = 'but'+ nextcol + i;
+        //console.log(myElement);
+        if (nextcol <= 8) {
+            const element = document.getElementById(myElement);
+            element.setAttribute('onclick', 'selectPick(this)');
+        }
+        //console.log(element);  
+    }
+    
 }
+
+// onclick='selectPick(this)'
 // build html cards
 for (let k=1; k<=8; k++) {
     for (let i=1; i<=8; i++) {
 
         myName = 'but'+k.toString()+i.toString();
         //console.log(myName);
-        myHtml =  "<div class='card' style='width: 6rem;'><div class='card-body'><h5 class='card-title'>" + i + "</h5><p class='card-text'></p><a href='#' id="+myName+" onclick='selectPick(this)' class='btn btn-secondary'>Select</a></div></div>";
+        myHtml =  "<div class='card' style='width: 6rem;'><div class='card-body'><h5 class='card-title'>" + i + "</h5><p class='card-text'></p><a href='#' id="+myName+"  class='btn btn-secondary'>Select</a></div></div>";
         myCol = "colxx"+k;
         let myText = document.getElementById(myCol);
         myText.innerHTML += myHtml;
     };
 };
+
+let buttonHome = document.querySelector(".btn");
+let CountButtonHomeClicks = 0;
+
+buttonHome.addEventListener("click", function() {
+  CountButtonHomeClicks += 1;
+  //console.log(CountButtonHomeClicks);
+});
